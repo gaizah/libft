f43075fa458d83bbebb4ec4ffa37395a22280803
Laurie DECKERS <ldeckers@e2r1p12.42.fr>
2015-01-08 21:18:01 +0100

    ft_strtrim.c pas fini

--- Libft_42/ft_strtrim.c
+++ Libft_42/ft_strtrim.c
@@ -6,14 +6,38 @@
 /*   By: ldeckers <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/07 23:37:48 by ldeckers          #+#    #+#             */
-/*   Updated: 2015/01/07 23:42:49 by ldeckers         ###   ########.fr       */
+/*   Updated: 2015/01/08 20:13:35 by ldeckers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s)
 {
-	
+	int i;
+	int j;
+	int len;
+	char *temp;
 
+	i = 0;
+	j = 0;
+	len = ft_strlen(s);
+	if (s == NULL)
+		return (NULL);
+	while (s[i] == '\n' || s[i] == '\t' || s[i] == ' ' && s[i] != '\0')
+		i++;
+	while  (s[len] != ' ' || s[len] != '\n' || s[len] != '\t')
+		len--;
+	temp = ft_strsub(s, j, (j - len));
+	return (temp);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		printf("%s\n", ft_strtrim(av[1]));
+	}
+	return (0);
 }
